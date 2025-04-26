97d352c098dd9c0627d6243277f98843959710d8
MuGrahiman <mujeebrahiman2000@gmail.com>
2025-04-26 21:08:11 +0530

    feat: Implement order filtration feature .

--- src/pages/order/UserOrderPage.jsx
+++ src/pages/order/UserOrderPage.jsx
@@ -6,22 +6,44 @@ import OrderEmptySpot from "./OrderEmptySpot";
 import useOrder from "../../hooks/useOrder";
 import LoadingTemplate from "../../components/LoadingTemplate";
 import InfoLayout from "../../components/InfoLayout";
+import TabSwitcher from "../../components/TabSwitcher";
 
 const UserOrderPage = () => {
 	const ROLE = "client";
 	// Mock data with default values
 	const { currentUserName } = useAuth(ROLE);
+	const TAB = {
+		ACTIVE: "active",
+		COMPLETED: "completed",
+		CANCELLED: "cancelled",
+	};
+	const [selectedTab, setSelectedTab] = useState(TAB.ACTIVE);
 
-	const { ordersData, isOrdersLoading } = useOrder(ROLE);
-
+	const { ordersData, isOrdersLoading, isOrdersLength, filterOrders } =
+		useOrder(ROLE);
+	const handleTabSelection = (key) => {
+		if (key === TAB.ACTIVE) filterOrders({ orderStatus: ["processing"] });
+		if (key === TAB.COMPLETED) filterOrders({ orderStatus: ["delivered"] });
+		if (key === TAB.CANCELLED) filterOrders({ orderStatus: ["cancelled"] });
+		setSelectedTab(key);
+	};
 	return (
 		<div className="max-w-4xl mx-auto p-4 sm:p-6">
 			<UserNameHeader userName={currentUserName} />
 
 			{isOrdersLoading ? (
 				<LoadingTemplate />
-			) : ordersData?.length > 0 ? (
+			) : isOrdersLength ? (
 				<InfoLayout title="Your Orders">
+					<TabSwitcher
+						tabs={[
+							{ label: "Active", key: TAB.ACTIVE },
+							{ label: "Completed", key: TAB.COMPLETED },
+							{ label: "Cancelled", key: TAB.CANCELLED },
+						]}
+						activeTab={selectedTab}
+						onSelectTab={handleTabSelection}
+					/>
 					<OrderList orders={ordersData} />
 				</InfoLayout>
 			) : (
